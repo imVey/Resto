2f96d4f3de8e36d0f6ad2e9818a2876c390bd301
imVey <marwanbella91@hotmail.fr>
2024-05-26 21:27:16 +0200

    Ajout système routage

---
+++ Router/Route.js
@@ -0,0 +1,8 @@
+export default class Route {
+  constructor(url, title, pathHtml, path35 = "") {
+    this.ulr = url;
+    this.title = title;
+    this.pathHtml = pathHtml;
+    this.path35 = path35;
+  }
+}
---
+++ Router/allRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./Route.js";
+
+// Définir ici nos routes
+
+export const allRoutes = [new Route("/", "Acceuil", "/pages/home.html")];
+
+// Le titre s'affiche comme ceci : Route.titre - website
+
+export const websiteName = "Quai antique";
